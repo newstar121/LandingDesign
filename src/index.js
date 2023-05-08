e23fc7e40c82188433141fcd8aa594123b4f4466
newstar121 <bg.newstar121@gmail.com>
2023-05-08 01:45:46 -0700

    CI=false

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-process.env.CI = false;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
